556e54c1e7ed62ec0a32b1769e30f5d9726e1fc5
T S Samarth <tssam1547@gmail.com>
2025-05-01 19:42:51 +0530

    forms using JS

--- Assesment1/Form3.js
+++ Assesment1/Form3.js
@@ -1,38 +1,55 @@
 function submitForm() {
-    let val = true;
-  
-    let nameInput = document.formName.nameName.value;
-    let numberInput = document.formName.numberName.value;
-    let ageInput = document.formName.age.value;
-  
-    if (nameInput.length < 3 || nameInput.length >= 20) {
-      val = false;
-      checkError("nameInput","The length of name should be greater than 3 and less than 20");
-    }
-  
-    if (numberInput.length != 10) {
-      val = false;
-      checkError("numberInput", "The length of number should be 10 digit");
-    }
+  let val = true;
 
-    if (ageInput <= 18) {
-      val = false;
-      checkError("ageInput", "Age should be above 18");
-    }
-  
-    return val;
+  let nameInput = document.formName.nameName.value;
+  let numberInput = document.formName.numberName.value;
+  let ageInput = document.formName.age.value;
+
+  if (nameInput.length < 3 || nameInput.length >= 20) {
+    val = false;
+    checkError("nameInput", "The length of name should be greater than 3 and less than 20");
   }
-  var id, descr;
-  function checkError(id, descr) {
-    if (id === "nameInput") {
-      document.getElementById("namespan").innerHTML = descr;
-    }
-  
-    if (id === "numberInput") {
-      document.getElementById("numberspan").innerHTML = descr;
-    }
-  
-    if (id === "ageInput") {
-      document.getElementById("agespan").innerHTML = descr;
-    }
+  else{
+    clearForm('namespan');
   }
+
+  if (numberInput.length != 10) {
+    val = false;
+    checkError("numberInput", "The length of number should be 10 digit");
+  }
+  else{
+    clearForm('numberspan');
+  }
+
+  if (ageInput <= 18) {
+    val = false;
+    checkError("ageInput", "Age should be above 18");
+  }
+  else{
+    clearForm('agespan');
+  }
+
+  return val;
+}
+var id, descr;
+function checkError(id, descr) {
+  
+  if (id === "nameInput") {
+    document.getElementById("namespan").innerHTML = descr;
+  }
+ 
+
+  if (id === "numberInput") {
+    document.getElementById("numberspan").innerHTML = descr;
+  }
+
+
+  if (id === "ageInput") {
+    document.getElementById("agespan").innerHTML = descr;
+  }
+  
+}
+
+function clearForm(id){
+  document.getElementById(id).innerHTML='';
+}
---
+++ Assesment1/exampleForm.js
@@ -0,0 +1,62 @@
+// function onClick() {
+//     document.getElementById('nameId').innerHTML='min 3 letter max 20 letetr';
+
+//     let dothis=document.getElementsByClassName('numberClass')[0].innerHTML='skanda';
+
+//     document.getElementsByName('nameName')[0].innerHTML='Helloooo';
+
+//     document.getElementsByTagName('tagName')[0].innerHTML='Sagar';
+
+// }
+
+
+
+function submitForm() {
+    let val = true;
+
+    let nameInput = document.formName.nameName.value;
+    // let ageInput = document.formName.ageAge.value;
+    let numberInput = document.formName.numberNumber.value;
+
+    if (nameInput.length < 3 || nameInput.length > 20) {
+        val = false;
+        setError('nameErrorId',"Name must be between 3 and 20 characters.");
+    }
+
+    if (numberInput.length != 10) {
+        val = false;
+        setError('numberErrorId',"Number must be exactly 10 digits.");
+    }
+
+    // if (ageInput < 18) {
+    //     val = false;
+    //     setError("Age must be 18 or above.");
+    // }
+
+    // if (!val) return false; // Stop submission if errors exist
+
+    // let emailId = document.formName.emailEmail.value;
+    // let genderId = document.formName.gender.value;
+    // let passwordId = document.formName.passwardPassward.value;
+    // let confirmId = document.formName.correctCorrect.value;
+
+    // if (passwordId !== confirmId) {
+    //     setError("Passwords do not match.");
+    //     return false;
+    // }
+
+    // let message = "Form Submitted Successfully!\n" +
+    //     "Name: " + nameInput + "\n" +
+    //     "Number: " + numberInput + "\n" +
+    //     "Email: " + emailId + "\n" +
+    //     "Gender: " + genderId + "\n" +
+    //     "Password: " + passwordId + "\n" +
+    //     "Confirm Password: " + confirmId + "\n" +
+    //     "Age: " + ageInput;
+
+    // alert(message);
+    return true;
+}
+function setError(id,message) {
+    document.getElementById(id).innerHTML=message;
+}
